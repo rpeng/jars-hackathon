894f13a57e8a5d064237b79a1de10c105aeb2bc1
Richard Peng <peng.richard@gmail.com>
2011-01-22 04:13:47 -0500

    Pushing all project files onto git

---
+++ main.py
@@ -0,0 +1,60 @@
+import os
+import datetime
+
+from google.appengine.api import users
+from google.appengine.ext import webapp
+from google.appengine.ext.webapp import template
+from google.appengine.ext.webapp.util import run_wsgi_app
+from google.appengine.ext import db
+
+class UserIdentity:
+    name = ""
+    protocol = ""
+    
+class Question(db.Model):
+
+    # here we define our database structure
+    
+    userID = db.StringProperty() 
+    userName = db.StringProperty()
+    Title = db.StringProperty()
+    Type = db.StringProperty()
+    Ques = db.StringProperty(multiline=True)
+    questionID = db.IntegerProperty()
+    tags = db.StringListProperty()
+    date = db.DateTimeProperty(auto_now_add=True)
+
+    
+class MainPage(webapp.RequestHandler):
+    def get(self):
+        
+        template_values = {}
+        self.response.out.write(template.render('index.html',template_values))
+
+class QuestionPage(webapp.RequestHandler):
+    def get(self):
+        template_values = {}
+        self.response.out.write(template.render('question.html',template_values))
+        
+class QnAPage(webapp.RequestHandler):
+    def get(self):
+        template_values = {}
+        self.response.out.write(template.render('qanda.html',template_values))
+
+application = webapp.WSGIApplication(
+                                     [
+                                         
+                                     ('/', MainPage),
+                                     ('/index.html', MainPage),
+                                     ('/question.html', QuestionPage),
+                                     ('/qanda.html', QnAPage)
+                                     
+                                     ],
+                        
+                                     debug=True)
+
+def main():
+    run_wsgi_app(application)
+
+if __name__ == "__main__":
+    main()
