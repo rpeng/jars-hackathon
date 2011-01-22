70c558f98c48be7375d19a3e0c2baba5fb697048
Richard Peng <peng.richard@gmail.com>
2011-01-22 07:25:29 -0500

    pushing

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ from google.appengine.ext.webapp import template
 from google.appengine.ext.webapp.util import run_wsgi_app
 from google.appengine.ext import db
 
+globalID = 1000
+
 class UserIdentity:
     name = ""
     protocol = ""
@@ -26,17 +28,24 @@ class Question(db.Model):
 
     
 class MainPage(webapp.RequestHandler):
+    questionID = 0
+    
     def get(self):
         
         template_values = {}
         self.response.out.write(template.render('index.html',template_values))
 
 class QuestionSubmit(webapp.RequestHandler):
+    
     def post(self):
+        global globalID
+    
         qData = Question()
         
         qData.Title = self.request.get('title')
-        qData.Question = self.request.get('question')
+        qData.Ques = self.request.get('q')
+        qData.questionID = globalID
+        globalID += 1
         qData.put()
         self.redirect("/qanda.html")
         
@@ -49,16 +58,16 @@ class QuestionPage(webapp.RequestHandler):
 
 class AnswerPage(webapp.RequestHandler):
     def get(self):
-        template_values = {}
-        self.response.out.write(template.render('alist.html',template_values))
-
-class QnAPage(webapp.RequestHandler):
-    def get(self):
         questions_query = Question.all()
         questions = questions_query.fetch(5)
         template_values = {
             'questions': questions
-            }
+        }
+        self.response.out.write(template.render('alist.html',template_values))
+
+class QnAPage(webapp.RequestHandler):
+    def get(self):
+        template_values = {}
         self.response.out.write(template.render('qanda.html',template_values))
 
 application = webapp.WSGIApplication(
