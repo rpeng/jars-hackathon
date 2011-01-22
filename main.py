ce4d1caf0f955e26c7f39ea79dd762c0f5070963
Richard Peng <peng.richard@gmail.com>
2011-01-22 08:27:52 -0500

    adfa

--- main.py
+++ main.py
@@ -16,16 +16,20 @@ class UserIdentity:
 class Question(db.Model):
 
     # here we define our database structure
-    
-    userID = db.StringProperty() 
+
     userName = db.StringProperty()
     Title = db.StringProperty()
-    Type = db.StringProperty()
     Ques = db.StringProperty(multiline=True)
     questionID = db.IntegerProperty()
-    tags = db.StringListProperty()
     date = db.DateTimeProperty(auto_now_add=True)
 
+class Answer(db.Model):
+    userName = db.StringProperty()
+    Title = db.StringProperty()
+    Ans = db.StringProperty(multiline=True)
+    answerID = db.IntegerProperty()
+    tags = db.StringListProperty()
+    date = db.DateTimeProperty(auto_now_add=True)
     
 class MainPage(webapp.RequestHandler):
     questionID = 0
@@ -47,14 +51,18 @@ class QuestionSubmit(webapp.RequestHandler):
         qData.questionID = globalID
         globalID += 1
         qData.put()
-        self.redirect("/qanda.html")
-        
-    pass
+        self.redirect("/qanda?"+str(qData.questionID))
+
         
 class QuestionPage(webapp.RequestHandler):
     def get(self):
         template_values = {}
         self.response.out.write(template.render('question.html',template_values))
+
+class HelpPage(webapp.RequestHandler):
+    def get(self):
+        template_values = {}
+        self.response.out.write(template.render('help.html',template_values))
 
 class AnswerPage(webapp.RequestHandler):
     def get(self):
@@ -63,11 +71,17 @@ class AnswerPage(webapp.RequestHandler):
         template_values = {
             'questions': questions
         }
+
         self.response.out.write(template.render('alist.html',template_values))
 
 class QnAPage(webapp.RequestHandler):
     def get(self):
-        template_values = {}
+        qId = self.request.query_string
+        qs = db.GqlQuery("select * from Question where questionID = "+qId)
+        template_values = {
+            'title':qs[0].Title,
+            'question':qs[0].Ques
+            }
         self.response.out.write(template.render('qanda.html',template_values))
 
 application = webapp.WSGIApplication(
@@ -77,8 +91,9 @@ application = webapp.WSGIApplication(
                                      ('/alist.html', AnswerPage),
                                      ('/index.html', MainPage),
                                      ('/question.html', QuestionPage),
-                                     ('/qanda.html', QnAPage),
-                                     ('/submitQuestion', QuestionSubmit)
+                                     ('/qanda*', QnAPage),
+                                     ('/submitQuestion', QuestionSubmit),
+                                     ('/help.html',HelpPage)
                                      ],
                         
                                      debug=True)
