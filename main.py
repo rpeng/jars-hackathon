87410e3b2b1a0a9edfe46348bec0e3a1616d0d07
Richard Peng <peng.richard@gmail.com>
2011-01-22 09:20:05 -0500

    Signed-off-by: Richard Peng <peng.richard@gmail.com>

--- main.py
+++ main.py
@@ -18,14 +18,14 @@ class Question(db.Model):
     # here we define our database structure
 
     userName = db.StringProperty()
+    email = db.StringProperty()
     Title = db.StringProperty()
     Ques = db.StringProperty(multiline=True)
     questionID = db.IntegerProperty()
-    date = db.DateTimeProperty(auto_now_add=True)
+    date = db.DateTimeProperty(auto_now=True)
 
 class Answer(db.Model):
     userName = db.StringProperty()
-    Title = db.StringProperty()
     Ans = db.StringProperty(multiline=True)
     answerID = db.IntegerProperty()
     tags = db.StringListProperty()
@@ -48,10 +48,28 @@ class QuestionSubmit(webapp.RequestHandler):
         
         qData.Title = self.request.get('title')
         qData.Ques = self.request.get('q')
+        qData.userName = self.request.get('n')
+        qData.email = self.request.get('email')
         qData.questionID = globalID
+        
         globalID += 1
         qData.put()
         self.redirect("/qanda?"+str(qData.questionID))
+        
+class AnswerSubmit(webapp.RequestHandler):
+    
+    def post(self):
+        global globalID
+    
+        aData = Answer()
+
+        aData.Ans = self.request.get('ans')
+        aData.userName = self.request.get('na')
+        aData.email = self.request.get('ema')
+        aData.answerID = self.request.query_string
+        
+        qData.put()
+        self.redirect("/qanda?"+str(aData.answerID))
 
         
 class QuestionPage(webapp.RequestHandler):
@@ -80,7 +98,10 @@ class QnAPage(webapp.RequestHandler):
         qs = db.GqlQuery("select * from Question where questionID = "+qId)
         template_values = {
             'title':qs[0].Title,
-            'question':qs[0].Ques
+            'question':qs[0].Ques,
+            'user':qs[0].userName,
+            'email':qs[0].email,
+            'date':qs[0].date.strftime("%m/%d/%Y %H:%M")
             }
         self.response.out.write(template.render('qanda.html',template_values))
 
@@ -93,6 +114,7 @@ application = webapp.WSGIApplication(
                                      ('/question.html', QuestionPage),
                                      ('/qanda*', QnAPage),
                                      ('/submitQuestion', QuestionSubmit),
+                                     ('/submitAnswer*',AnswerSubmit),
                                      ('/help.html',HelpPage)
                                      ],
                         
